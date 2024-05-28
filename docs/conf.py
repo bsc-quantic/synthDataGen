d4a2f49e521219f5e26efac88d06d34be4413f53
Germán Navarro <ge.najim@gmail.com>
2024-05-28 15:50:50 +0200

    Update version for release 0.2

--- docs/conf.py
+++ docs/conf.py
@@ -10,10 +10,10 @@ sys.path.insert(0, os.path.abspath('..'))
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'Synthetic Data Generator for Energy Forecast'
+project = 'synthDataGen'
 copyright = '2023, Santiago Fernández Prieto & Germán Navarro. \nDesarrollado en el marco del Proyecto CUCO (https://www.cuco.tech/ subvencionado por CDTI y con el apoyo del Ministerio de Ciencia e Innovación bajo el Plan de Recuperación, Transformación y Resiliencia.'
 author = 'Santiago Fernández Prieto & Germán Navarro'
-release = '0.1'
+release = '0.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
